aa9633ae9a6d6c525d5a3b7ff1b5b4f46140ab45
FrankXuxe <89552517+FrankXuxe@users.noreply.github.com>
2021-10-31 18:36:42 -0500

    Add files via upload

--- nlpHW.py
+++ nlpHW.py
@@ -4,11 +4,11 @@ from textblob import TextBlob
 import nltk
 from pathlib import Path
 import pandas as pd
-#import imageio
-#from wordcloud import WordCloud
-#import wordcloud
-
-blob = TextBlob(Path("bookofJohn.txt").read_text())
+import imageio
+from wordcloud import WordCloud
+import wordcloud
+mask_image = imageio.imread("mask_rectangle.png")
+blob = TextBlob(Path("book of John text.txt").read_text())
 stops = stopwords.words("english")
 more_stops = ['thy', 'ye', 'verily', 'thee', 'hath', 'say', 'thou', 'art', 'shall']
 stops += more_stops
@@ -19,33 +19,23 @@ clean_items = [i for i in items if i[0] not in stops]
 
 sorted_list = sorted(clean_items, key=itemgetter(1), reverse=True)
 
-top15 = sorted_list[:15]
+top15 = sorted_list[:16]
 
 new_top15 = ''
 
 for i in top15:
         new_top15 += i[0] + ' '
 
-print(new_top15)
+print(top15)
 
-'''
+
 text = Path('book of John text.txt').read_text()
-outfile = open('book of John text.txt', 'w')
 
-wordcloud = WordCloud(colormap='prism', 
+wordcloud = WordCloud(colormap='winter', mask= mask_image,
                       background_color='grey')
 
-for line in text:
-    if line not in line:
-            line = line.replace(i, '')
-        
-outfile.write(line)
-
-wordcloud = wordcloud.generate(text)
+wordcloud = wordcloud.generate(new_top15)
 
 wordcloud = wordcloud.to_file("BOJ.png")
 
-outfile. close()
-
 print("done")
-'''
